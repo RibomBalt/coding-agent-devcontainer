f4910978a3274f1395625109dc5e3d8ecb6fb78e
RibomBalt <ribombalt1@gmail.com>
2026-08-14 15:44:37 +0800

    feat: use git delta

--- src/git-delta/install.sh
+++ src/git-delta/install.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
+USERNAME="${_REMOTE_USER:-node}"
+USER_HOME="${_REMOTE_USER_HOME:-$(getent passwd "$USERNAME" | cut -d: -f6)}"
+FEATURE_DIR="$(cd "$(dirname "$0")" && pwd)"
 GIT_DELTA_VERSION="${GIT_DELTA_VERSION:-0.18.2}"
 
 ARCH="$(dpkg --print-architecture)"
 wget "https://github.com/dandavison/delta/releases/download/${GIT_DELTA_VERSION}/git-delta_${GIT_DELTA_VERSION}_${ARCH}.deb"
 dpkg -i "git-delta_${GIT_DELTA_VERSION}_${ARCH}.deb"
 rm "git-delta_${GIT_DELTA_VERSION}_${ARCH}.deb"
+
+su -l -s /bin/bash -c \
+    "git config --global core.pager delta && \
+     git config --global interactive.diffFilter \"delta --color-only\" && \
+     git config --global delta.navigate true" \
+    "$USERNAME"
--- test/git-delta/test.sh
+++ test/git-delta/test.sh
@@ -4,5 +4,8 @@ set -e
 echo "==> Verifying git-delta feature"
 
 command -v delta || exit 1
+git config --global core.pager | grep -qx delta || exit 1
+git config --global interactive.diffFilter | grep -qx "delta --color-only" || exit 1
+git config --global delta.navigate | grep -qx true || exit 1
 
 echo "✓ git-delta OK"
