eb6f8bfeb0742975852fd2918743d1ed16835ecb
RibomBalt <ribombalt1@gmail.com>
2026-08-23 21:18:02 +0800

    feat: label base image to cache build

--- image/build-image.sh
+++ image/build-image.sh
@@ -5,11 +5,15 @@ set -euo pipefail
 WSL_CLASH_PROXY="http://host.docker.internal:7890"
 DIR="$(dirname "$0")"
 IMAGE=${IMAGE:-"opencode-sandbox-ribom:latest"}
+SOURCE_HASH_LABEL="devcontainer.base-source-hash"
 
 pushd "$DIR"
 trap 'popd' EXIT
 
+SOURCE_HASH=$(git -C "$(git rev-parse --show-toplevel)" ls-files image/ | sort | xargs sha256sum | sha256sum | cut -d' ' -f1)
+
 docker build -f Dockerfile-base -t "$IMAGE" \
+    --label "${SOURCE_HASH_LABEL}=${SOURCE_HASH}" \
     --build-arg TZ="${TZ:-Asia/Shanghai}" \
     --build-arg NODE_VERSION="${NODE_VERSION:-24.19.0}" \
     --build-arg NODE_MIRROR="${NODE_MIRROR:-https://registry.npmmirror.com/-/binary/node}" \
