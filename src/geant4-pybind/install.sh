e11c9a75afb1bee3e4f48162c3f523a283ed20ad
RibomBalt <ribombalt1@gmail.com>
2026-08-23 23:09:57 +0800

    fix: user home in geant4 install

--- src/geant4-pybind/install.sh
+++ src/geant4-pybind/install.sh
@@ -5,5 +5,5 @@ USERNAME="${_REMOTE_USER:-node}"
 USER_HOME="${_REMOTE_USER_HOME:-$(getent passwd "$USERNAME" | cut -d: -f6)}"
 FEATURE_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-mkdir -p ~/.geant4_pybind && chown -R $USERNAME:$USERNAME ~/.geant4_pybind
-cp "$FEATURE_DIR/download-geant4-pybind-data.sh" ~/.local/bin/ && chmod +x ~/.local/bin/download-geant4-pybind-data.sh
+mkdir -p $USER_HOME/.geant4_pybind && chown -R $USERNAME:$USERNAME $USER_HOME/.geant4_pybind
+cp "$FEATURE_DIR/download-geant4-pybind-data.sh" $USER_HOME/.local/bin/ && chmod +x $USER_HOME/.local/bin/download-geant4-pybind-data.sh
